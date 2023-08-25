ea24266d5b92d4123d54465d9ad8d31fca7e9f0f
EugenePonomarev <129969020+EugenePonomarev@users.noreply.github.com>
2023-08-25 17:56:35 +0300

    Update Constants.kt

--- app/src/main/java/com/crazycat/weather/ui/Constants.kt
+++ app/src/main/java/com/crazycat/weather/ui/Constants.kt
@@ -2,6 +2,7 @@ package com.crazycat.weather.ui
 
 object Constants {
     var BASE_URL: String = "https://api.openweathermap.org/data/2.5/"
+    //Key - 4e59726f61f8b17b06e8daa7605a24b3
     const val API_KEY = ""
     const val PERMISSION_REQUEST_CODE = 123
 
